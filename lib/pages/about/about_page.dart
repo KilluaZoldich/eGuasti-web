b409cad7d12bcb2c29777670144b2f71ab1ba28f
KilluaZoldich <metinsuseven@gmail.com>
2025-03-02 23:48:21 +0100

    Deploy web app to GitHub Pages

--- lib/pages/about/about_page.dart
+++
@@ -1,7 +0,0 @@
-Widget buildAuthor(BuildContext context) {
-    return ListTile(
-      leading: const Text(""),
-      title: Text(AppLocalizations.of(context).aboutAuthorTitle),
-      subtitle: const Text("KilluaZoldich\nBased on original work by Alberto Pedron"),
-    );
-  }
